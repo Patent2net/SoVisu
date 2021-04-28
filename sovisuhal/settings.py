f1b141657db05daf8a88b9a7ac39cf8cd7aaf361
Patent2net <patent2net@gmail.com>
2021-04-28 17:00:09 +0200

    Update settings.py

--- sovisuhal/settings.py
+++ sovisuhal/settings.py
@@ -17,7 +17,7 @@ from decouple import config
 
 mode = 'Prod'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
