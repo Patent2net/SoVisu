45922a621ffdaeca1604bb90bad951ad6f8b7b16
Alaric Tabariès <alarictabaries@gmail.com>
2021-04-29 19:55:22 +0200

    domaine orienteurs côté utilisateur, to-do : ajout côté serveur

--- get_domains_from_aurehal.py
+++ get_domains_from_aurehal.py
@@ -34,6 +34,56 @@ for input in inputs:
     dom['id'] = input.get('value')
     dom['label_en'] = getLabel(input.get('value'), 'en')
     dom['label_fr'] = getLabel(input.get('value'), 'fr')
+    dom['children'] = []
     domains.append(dom)
 
-print(domains)
+tree = {'id': 'Concepts', 'children': []}
+
+for dom in domains:
+    sid = dom['id'].split('.')
+
+    if len(sid) == 1:
+        exists = False
+        for child in tree['children']:
+            if sid[0] == child['id']:
+                exists = True
+        if not exists:
+            tree['children'].append(dom)
+
+    if len(sid) == 2:
+        exists = False
+        for child in tree['children']:
+            if sid[0] == child['id']:
+                if 'children' in child:
+                    for child1 in child['children']:
+                        if sid[0] + '.' + sid[1] == child1['id']:
+                            exists = True
+
+        if not exists:
+            for child in tree['children']:
+                if 'children' in child:
+                    if sid[0] == child['id']:
+                        child['children'].append(dom)
+
+    if len(sid) == 3:
+        exists = False
+        for child in tree['children']:
+            if sid[0] == child['id']:
+                if 'children' in child:
+                    for child1 in child['children']:
+                        if sid[0] + '.' + sid[1] == child1['id']:
+                            if 'children' in child1:
+                                for child2 in child1['children']:
+                                    if sid[0] + '.' + sid[1] + '.' + sid[2] == child2['id']:
+                                        exists = True
+
+        if not exists:
+            for child in tree['children']:
+                if 'children' in child:
+                    if sid[0] == child['id']:
+                        for child1 in child['children']:
+                            if sid[0] + '.' + sid[1] == child1['id']:
+                                child1['children'].append(dom)
+
+
+print(tree)
