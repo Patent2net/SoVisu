06c811e9e98f2aca880f84e6c7cb2f856d782238
Patent2net <patent2net@gmail.com>
2021-05-26 18:35:18 +0200

    indexations des documents HAL pour un chercheur - les docs sont indexés - les concepts ne sont pas retrouvés - le retour ne restitue pas l'ORCid tentative d'ajout d'une barre de progression (actuellement joliment statique car il faut installer redis et celery... chui pas trop trop chaud)

--- sovisuhal/urls.py
+++ sovisuhal/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from django.conf.urls import include
-
+from django.urls import re_path, include
 from . import views
 
 urlpatterns = [
@@ -53,6 +53,6 @@ urlpatterns = [
     path('unknown/', views.unknown, name='unknown'),
     path('create/', views.create, name='creation'),
     path('accounts/', include('uniauth.urls', namespace='uniauth')),
-    path('loggedin/', views.loggedin, name='loggued')
-
+    path('loggedin/', views.loggedin, name='loggued'),
+    path('celery-progress/', include('celery_progress.urls'))  # the endpoint is configurable
 ]
