cde92fda8884a8cf2d62649120707812fcdc43a9
Patent2net <patent2net@gmail.com>
2022-04-15 17:03:42 +0200

    inversion du hack provisoire....

--- sovisuhal/settings.py
+++ sovisuhal/settings.py
@@ -145,7 +145,9 @@ DATABASES = {
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
- 
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
