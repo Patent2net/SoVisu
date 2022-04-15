f3d132260e5911fc2eb1397e7b7bc0c41b5abf73
Patent2net <patent2net@gmail.com>
2022-04-15 16:58:30 +0200

    suppression tempo du check des mdp bidon

--- sovisuhal/settings.py
+++ sovisuhal/settings.py
@@ -145,9 +145,7 @@ DATABASES = {
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
+ 
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
