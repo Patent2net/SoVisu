6e41fe81c9c3c6aeab18f97aa76d3fa86e8627e5
baltican <62282795+baltican@users.noreply.github.com>
2022-05-25 11:16:16 +0200

    coquille dans script en manuel

--- elasticHal/collect_from_HAL.py
+++ elasticHal/collect_from_HAL.py
@@ -382,4 +382,4 @@ def collect_data(laboratories, researcher, csv_enabler=True, django_enabler=None
 
 
 if __name__ == '__main__':
-    collect_data(laboratories=None, researcher='on')
+    collect_data(laboratories='on', researcher='on')
