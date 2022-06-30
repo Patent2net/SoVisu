b4cfa1f408b72320ec2993ec36f5dc7dc998f79c
E-Bara <e-bara@outlook.com>
2022-06-30 17:12:11 +0200

    changement de méthode de récupération pour la mise à jour via admin.py de elastic ("peupler elastic" dans laboratories, à renommer)

--- elasticHal/views.py
+++ elasticHal/views.py
@@ -1,3 +1,20 @@
 # from django.shortcuts import render
-
+from sovisuhal.libs import esActions
+es = esActions.es_connector()
 # Create your views here.
+# Set choices to an empty list as it is a required argument.
+
+
+def get_index_list():
+    scope_param = esActions.scope_all()
+    count = es.count(index="*-laboratories", body=scope_param, request_timeout=50)['count']
+    result = es.search(index="*-laboratories", body=scope_param, size=count,
+                       filter_path=["hits.hits._index, hits.hits._source.acronym"])
+    result = result['hits']['hits']
+    print(f"result = {result}")
+    indexes = []
+    for lab in result:
+        indexes.append((lab["_index"], lab["_source"]["acronym"]))
+    indexes = tuple(indexes)
+    print(f"indexes2 content {indexes}")
+    return indexes
