55408345ff25a3307f9b32c2e1ac0e3ed682ff4d
Alaric <alarictabaries@gmail.com>
2022-07-01 12:06:47 +0200

    gestion des validated

---
+++ workers/backup_validated.py
@@ -0,0 +1,21 @@
+from sovisuhal.libs import esActions
+import json
+
+
+es = esActions.es_connector()
+
+q = {"query": {"match": {"validated": True}}}
+count = es.count(index="*-documents", body=q)['count']
+docs = es.search(index="*-documents", body=q, size=count)
+r_count = docs["hits"]["total"]["value"]
+
+print("found " + str(r_count) + "/" + str(count) + " docs")
+
+dump = []
+
+for doc in docs["hits"]["hits"][0:5]:
+    dump.append({"docid": doc["_id"], "index": doc["_index"]})
+
+
+with open('data/dump.json', 'w') as outfile:
+    json.dump(dump, outfile)
