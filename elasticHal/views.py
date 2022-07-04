a68b4c4198513de14ec099232caa1ce5309bcd7f
E-Bara <e-bara@outlook.com>
2022-07-04 19:26:51 +0200

    Affichage de structAcronym avec le Labo (lancer add_structAcronym_key.py dans doc avant)

--- elasticHal/views.py
+++ elasticHal/views.py
@@ -9,11 +9,11 @@ def get_index_list():
     scope_param = esActions.scope_all()
     count = es.count(index="*-laboratories", body=scope_param, request_timeout=50)['count']
     if count > 0:
-        result = es.search(index="*-laboratories", body=scope_param, size=count,
-                           filter_path=["hits.hits._index, hits.hits._source.acronym"])
+        result = es.search(index="*-laboratories", body=scope_param, size=count)
         result = result['hits']['hits']
+        print(result)
         indexes = []
         for lab in result:
-            indexes.append((lab["_index"], lab["_source"]["acronym"]))
+            indexes.append((lab["_index"], lab["_source"]["acronym"]+' (' + lab["_source"]["structAcronym"] + ')'))
         indexes = tuple(indexes)
     return indexes
