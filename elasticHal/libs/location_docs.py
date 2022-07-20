b7f2cb2d8a5ea050fee76a206fe965cbcfed5de4
E-Bara <e-bara@outlook.com>
2022-07-20 16:28:48 +0200

    Suppression de def extract_locations_from_docid_list dans location_docs.py

--- elasticHal/libs/location_docs.py
+++ elasticHal/libs/location_docs.py
@@ -2,7 +2,11 @@ import requests
 
 
 def generate_countrys_fields(doc):
-
+    """
+    Enrichi les données avec les champs "deptStructCountry_s", "labStructCountry_s", "structCountry_s", "structCountry_t","rgrpInstStructCountry_s", "rgrpLabStructCountry_s" en se basant sur les données du document
+    :param doc: document à enrichir
+    :return: document enrichi
+    """
     facet_fields_list = ["deptStructCountry_s", "labStructCountry_s", "structCountry_s", "structCountry_t",
                          "rgrpInstStructCountry_s", "rgrpLabStructCountry_s", ]
     country_list = list()
@@ -43,41 +47,4 @@ def generate_countrys_fields(doc):
     if len(country_list_upper) == 0:
         country_list_upper = [""]
     # print(" fin de génération des champs country")
-    return country_list_upper
-
-
-def extract_locations_from_docid_list(docid):
-    facet_fields_list = ["country_s", "deptStructCountry_s", "labStructCountry_s", "location",
-                         "rgrpInstStructCountry_s", "rgrpLabStructCountry_s", "rteamStructCountry_s",
-                         "deptStructAddress_s", "instStructCountry_s", "structCountry_s", "structCountry_t"]
-    url = 'https://api.archives-ouvertes.fr/search/?q=docid:"' + str(docid) + '"&wt=json&indent=true'
-    for facet_field in facet_fields_list:
-        url = url + "&facet=true&facet.field=" + facet_field
-        # https://api.archives-ouvertes.fr/search/?q=docid:854050&wt=json&indent=true&facet=true&facet.field=publicationLocation_s
-
-    res = requests.get(url, timeout=50).json()
-
-    country_list = list()
-    for facet_field in facet_fields_list:
-        pre_element = []
-        try:
-            if facet_field in res["facet_counts"]["facet_fields"]:
-                for element in res["facet_counts"]["facet_fields"][facet_field]:
-                    try:
-                        if int(element) == 1:
-                            country_list.append(pre_element)
-                        else:
-                            pass
-
-                    except:
-                        pass
-                    pre_element = element
-            else:
-                print("ERROR" + facet_field)
-        except Exception as e:
-            print(e)
-
-    country_list = list(set(country_list))
-    country_list_upper = [country.upper() for country in country_list]
-
     return country_list_upper
