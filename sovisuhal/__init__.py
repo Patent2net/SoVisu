d46e2f2c1540a271dbfa7cfcb1ba0807a516964d
Patent2net <patent2net@gmail.com>
2022-11-12 10:23:05 +0100

    if __name__ == '__main__': est rajouté conformément au warning rtd pour autodoc. MAis çà ne fonctionne pas mieux ici...

--- sovisuhal/__init__.py
+++ sovisuhal/__init__.py
@@ -1,4 +1,4 @@
 from __future__ import absolute_import, unicode_literals
 from elasticHal.celery import app as celery_app
-if __name__ == '__main__':
-    __all__ = ('celery_app',)
+
+__all__ = ('celery_app',)
