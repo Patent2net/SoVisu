ac727ec4e3d59e1a785836d3f5a98a12af05b359
Patent2net <patent2net@gmail.com>
2023-02-14 08:03:01 +0100

    pas sûr que ce soit utile

--- elasticHal/admin.py
+++ elasticHal/admin.py
@@ -26,6 +26,9 @@ from celery_progress.backend import ProgressRecorder
 
 admin.site.site_header = "Administration de SoVisu"
 
+# Celery tasks
+# task_id1 = task_id2 = task_id3 = None
+
 
 class ExportCsv:
     def export_as_csv(self, request, queryset):
@@ -153,8 +156,8 @@ class ElasticActions:
                         )
                         taches.append([ind, result1.task_id, result2.task_id])
 
-                elif collectionLabo == True:
-                    if chercheurs == True:
+                elif collectionLabo is True:
+                    if chercheurs is True:
                         laboratoire = collection.split("-")[1]
                         structure = collection.split("-")[0]
                         result1 = collect_laboratories_data2.delay(laboratoire)
