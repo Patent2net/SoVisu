ee0e8b322ea4066d0d750562e50c05f5604539b6
Patent2net <patent2net@gmail.com>
2023-02-14 08:02:16 +0100

    pas sûr que ce soit utile

--- elasticHal/__init__.py
+++ elasticHal/__init__.py
@@ -1,4 +1,4 @@
 from __future__ import absolute_import, unicode_literals
-from elasticHal.celery import app as celery_elasticHal_app
-# je sais pas pourquopi passé à l'as dans le git pull
-__all__ = ('celery_elasticHal_app',)
+from elasticHal.celery import app as celery_app
+
+__all__ = ("celery_app",)
