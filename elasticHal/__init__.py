8bab8c6c3d0d65aaa36a764f2f4093b1fc0d1fec
E-Bara <e-bara@outlook.com>
2023-02-20 17:01:08 +0100

    [normalisation] pre-commit elasticHal sauf admin.py

--- elasticHal/__init__.py
+++ elasticHal/__init__.py
@@ -1,4 +1,4 @@
-from __future__ import absolute_import, unicode_literals
 from elasticHal.celery import app as celery_elasticshal_app
+
 # je sais pas pourquopi passé à l'as dans le git pull
-__all__ = ('celery_elasticshal_app',)
+__all__ = ("celery_elasticshal_app",)
