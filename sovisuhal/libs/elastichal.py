b3d833651a45c469ff78a6b052eec0595c3707d6
E-Bara <62282795+E-Bara@users.noreply.github.com>
2023-07-26 01:01:13 +0200

    Correct collecte_docs and delete collecte_docs_old in elastichal.py

--- sovisuhal/libs/elastichal.py
+++ sovisuhal/libs/elastichal.py
@@ -8,7 +8,6 @@ from celery import shared_task
 from celery_progress.backend import ProgressRecorder
 from dateutil.relativedelta import relativedelta
 from decouple import config
-from django.shortcuts import redirect
 from elasticsearch import helpers
 from ldap3 import ALL, Connection, Server
 
@@ -23,7 +22,6 @@ from elasticHal.libs import (
 
 )
 from elasticHal.libs.archivesOuvertes import get_aurehalId, get_concepts_and_keywords
-
 from . import esActions
 
 # from uniauth.decorators import login_required
@@ -33,6 +31,7 @@ mode = config("mode")  # Prod --> mode = 'Prod' en env Var
 
 # Connect to DB
 es = esActions.es_connector()
+
 
 # TODO: Renommer Get_dico pour quelque chose de plus explicite
 # TODO: Revoir la fonction pour débloquer la création de profils
@@ -99,8 +98,10 @@ def indexe_chercheur(structid, ldapid, labo_accro, labhalid, idhal, idref, orcid
     if suppan_id != ldapid:
         print("aille", ldapid, " --> ", ldapid)
 
-    emploi = dico["attributes"]["typeEmploi"] if len(dico["attributes"]["typeEmploi"]) > 0 else [""] # TODO: Revoir
-    mail = dico["attributes"]["mail"] if len(dico["attributes"]["mail"]) > 0 else [""] # TODO: revoir
+    emploi = dico["attributes"]["typeEmploi"] if len(dico["attributes"]["typeEmploi"]) > 0 else [
+        ""]  # TODO: Revoir
+    mail = dico["attributes"]["mail"] if len(dico["attributes"]["mail"]) > 0 else [
+        ""]  # TODO: revoir
 
     supann_affect = []
     if "supannAffectation" in dico["attributes"].keys():
@@ -123,8 +124,10 @@ def indexe_chercheur(structid, ldapid, labo_accro, labhalid, idhal, idref, orcid
         "mail": mail[0],  # TODO: Intéret?
         "orcId": orcid,
         "lab": labo_accro,  # TODO: INTERET DE CETTE KEY?
-        "supannAffectation": ";".join(supann_affect),  # TODO: faire sauter pour créer un document structure à la place?
-        "supannEntiteAffectationPrincipale": supann_princ,  # TODO: faire sauter pour créer un document structure à la place?
+        "supannAffectation": ";".join(supann_affect),
+        # TODO: faire sauter pour créer un document structure à la place?
+        "supannEntiteAffectationPrincipale": supann_princ,
+        # TODO: faire sauter pour créer un document structure à la place?
         "firstName": searcher_data["firstName_s"],
         "lastName": searcher_data["lastName_s"],
         "structSirene": structid,
@@ -132,7 +135,8 @@ def indexe_chercheur(structid, ldapid, labo_accro, labhalid, idhal, idref, orcid
         "validated": False,
         "ldapId": ldapid,
         "Created": datetime.datetime.now().isoformat(),
-        "idhal": idhal,  # sert de clé pivot entre les docs, il faut être sûr que ce champ n'existe dans aucune des docs que l'on pourrait indexer
+        "idhal": idhal,
+        # sert de clé pivot entre les docs, il faut être sûr que ce champ n'existe dans aucune des docs que l'on pourrait indexer
         "halId_s": idhal,
         "aurehalId": aurehal,
         "idRef": idref,
@@ -164,16 +168,14 @@ def collecte_docs(self, entite):  # self,
     """
     doc_progress_recorder = ProgressRecorder(self)
     new_documents = []
-    index = "sovisu"
-
 
     # look hal.find_publication for full base list of keys.
-    if entite ["sovisu_category"] == 'searcher':
-        idhal = entite ["idhal"]
+    if entite["sovisu_category"] == 'searcher':
+        idhal = entite["idhal"]
         docs = hal.find_publications(idhal, "authIdHal_s")
 
     else:
-        idhal = entite ["idhal"]
+        idhal = entite["idhal"]
         docs = hal.find_publications(idhal, "labStructId_i")
     for num, doc in enumerate(docs):
         # Check if the document already exist in elastic for the searcher.
@@ -181,9 +183,9 @@ def collecte_docs(self, entite):  # self,
         # if not, it create the document, append it to new_documents and then helpers.bulk
         changements = False
         elastic_doc_id = f"{idhal}.{doc['halId_s']}"
-        document_exist = es.exists(index=index, id=elastic_doc_id)
+        document_exist = es.exists(index=SV_INDEX, id=elastic_doc_id)
         if document_exist:
-            existing_document = es.get(index=index, id=elastic_doc_id)
+            existing_document = es.get(index=SV_INDEX, id=elastic_doc_id)
             existing_document = existing_document["_source"]
 
             doc["MDS"] = utils.calculate_mds(doc)
@@ -203,7 +205,7 @@ def collecte_docs(self, entite):  # self,
             doc["sovisu_validated"] = True
 
             # Calcul de l'autorat du chercheur
-            if entite ["sovisu_category"] == 'searcher':
+            if entite["sovisu_category"] == 'searcher':
                 authorship = ""
                 # TODO: Revoir pour être plus fiable?
                 if doc["authIdHal_s"].index(idhal) == 0:
@@ -246,110 +248,21 @@ def collecte_docs(self, entite):  # self,
         else:
             doc["_id"] = elastic_doc_id
             new_documents.append(doc)
-        doc_progress_recorder.set_progress(num, len(docs), str(num) + " sur " + str(len(docs)) + " documents")
+        doc_progress_recorder.set_progress(num, len(docs),
+                                           str(num) + " sur " + str(len(docs)) + " documents")
 
     for indi in range(int(len(new_documents) // 50) + 1):
         boutdeDoc = new_documents[indi * 50: indi * 50 + 50]
         helpers.bulk(
             es,
             boutdeDoc,
-            index= index,
+            index=SV_INDEX,
         )
 
-    doc_progress_recorder.set_progress(num, len(docs), str(num) + " sur " + str(len(docs)) + " indexés")
+    doc_progress_recorder.set_progress(num, len(docs),
+                                       str(num) + " sur " + str(len(docs)) + " indexés")
     return "add publications done"
 
-# @shared_task(bind=True)
-# def collecte_docs_old(self, chercheur, overwrite=False):  # self,
-#     """
-#     Collecte les notices liées à un chercheur
-#     "overwrite" : remet les valeurs pour l'ensemble du document à ses valeurs initiales.
-#     """
-#     progress_recorder = ProgressRecorder(self)
-#     new_documents = []
-#     idhal = chercheur["idhal"]
-#     docs = hal.find_publications(idhal, "authIdHal_s")
-#
-#     progress_recorder.set_progress(0, len(docs), description="récupération des données HAL")
-#     # Insert documents collection
-#
-#     for num, doc in enumerate(docs):
-#         # Check if the document already exist in elastic for the searcher.
-#         # If yes, it update values depending if the mds changed or not and then es.updated
-#         # if not, it create the document, append it to new_documents and then helpers.bulk
-#         changements = False
-#         elastic_doc_id = f"{idhal}.{doc['halId_s']}"
-#         document_exist = es.exists(index=SV_INDEX, id=elastic_doc_id)
-#         if document_exist:
-#             existing_document = es.get(index=SV_INDEX, id=elastic_doc_id)
-#             existing_document = existing_document["_source"]
-#
-#             doc["MDS"] = utils.calculate_mds(doc)
-#
-#             if doc["MDS"] != existing_document["MDS"]:
-#                 # SI le MDS a changé alors modif qualitative sur la notice
-#                 changements = True
-#             else:
-#                 doc = existing_document
-#
-#         else:
-#             doc["records"] = []
-#             doc["sovisu_category"] = "notice"
-#             doc["sovisu_referentiel"] = "hal"
-#             doc["idhal"] = idhal,  # l'Astuce du
-#             doc["sovisu_id"] = f'{idhal}.{doc["halId_s"]}'
-#             doc["sovisu_validated"] = True
-#
-#             # Calcul de l'autorat du chercheur
-#             authorship = ""
-
-#             if doc["authIdHal_s"].index(idhal) == 0:
-#                 authorship = "firstAuthor"
-#             if doc["authIdHal_s"].index(idhal) == len(doc["authIdHal_s"]) - 1:
-#                 authorship = "lastAuthor"
-#
-#             doc["sovisu_authorship"] = authorship
-#
-#         if not document_exist or changements:
-#             location_docs.generate_countrys_fields(doc)
-#             doc = doi_enrichissement.docs_enrichissement_doi(doc)
-#             if "fr_abstract_s" in doc.keys():
-#                 if isinstance(doc["fr_abstract_s"], list):
-#                     doc["fr_abstract_s"] = "/n".join(doc["fr_abstract_s"])
-#                 if len(doc["fr_abstract_s"]) > 100:
-#                     doc["fr_entites"] = keyword_enrichissement.return_entities(
-#                         doc["fr_abstract_s"], "fr"
-#                     )
-#                     doc["fr_teeft_keywords"] = keyword_enrichissement.keyword_from_teeft(
-#                         doc["fr_abstract_s"], "fr"
-#                     )
-#             if "en_abstract_s" in doc.keys():
-#                 if isinstance(doc["en_abstract_s"], list):
-#                     doc["en_abstract_s"] = "/n".join(doc["en_abstract_s"])
-#                 if len(doc["en_abstract_s"]) > 100:
-#                     doc["en_entites"] = keyword_enrichissement.return_entities(doc["en_abstract_s"],
-#                                                                                "en")
-#                     doc["en_teeft_keywords"] = keyword_enrichissement.keyword_from_teeft(
-#                         doc["en_abstract_s"], "en")
-#             # Nouveau aussi ci dessous
-#             doc["MDS"] = utils.calculate_mds(doc)
-#             doc["Created"] = datetime.datetime.now().isoformat()
-#
-#         # on recalcule à chaque collecte... pour màj
-#         doc["postprint_embargo"], doc["preprint_embargo"] = should_be_open(doc)
-#
-#         if document_exist:
-#             es.update(index=SV_INDEX, id=elastic_doc_id, doc=doc, refresh="wait_for")
-#         else:
-#             doc["_id"] = elastic_doc_id
-#             new_documents.append(doc)
-#         progress_recorder.set_progress(num, len(docs), description="(récolte)")
-#
-#     helpers.bulk(es, new_documents, index=SV_INDEX, refresh="wait_for")
-#
-#     progress_recorder.set_progress(num, len(docs), description="(indexation)")
-#     return chercheur
-#
 
 # TODO: intégrer dans utils.py après modification du module elasticHal?
 def should_be_open(notice):
@@ -361,17 +274,17 @@ def should_be_open(notice):
     # SHERPA/RoMEO embargo
     notice["postprint_embargo"] = None
     if (
-        "fileMain_s" not in notice
-        or notice["openAccess_bool"] is False
-        or "linkExtUrl_s" not in notice
+            "fileMain_s" not in notice
+            or notice["openAccess_bool"] is False
+            or "linkExtUrl_s" not in notice
     ):
         if "journalSherpaPostPrint_s" in notice:
             if notice["journalSherpaPostPrint_s"] == "can":
                 notice["postprint_embargo"] = "false"
             elif (
-                notice["journalSherpaPostPrint_s"] == "restricted"
-                and "publicationDate_tdate" in notice
-                and "journalSherpaPostRest_s" in notice
+                    notice["journalSherpaPostPrint_s"] == "restricted"
+                    and "publicationDate_tdate" in notice
+                    and "journalSherpaPostRest_s" in notice
             ):
                 matches = re.finditer(
                     r"(\S+\s+){2}(?=embargo)", notice["journalSherpaPostRest_s"].replace("[", " ")
@@ -398,16 +311,16 @@ def should_be_open(notice):
 
     notice["preprint_embargo"] = None
     if (
-        "fileMain_s" not in notice
-        or notice["openAccess_bool"] is False
-        or "linkExtUrl_s" not in notice
+            "fileMain_s" not in notice
+            or notice["openAccess_bool"] is False
+            or "linkExtUrl_s" not in notice
     ):
         if "journalSherpaPrePrint_s" in notice:
             if notice["journalSherpaPrePrint_s"] == "can":
                 notice["preprint_embargo"] = "false"
             elif (
-                notice["journalSherpaPrePrint_s"] == "restricted"
-                and "journalSherpaPreRest_s" in notice
+                    notice["journalSherpaPrePrint_s"] == "restricted"
+                    and "journalSherpaPreRest_s" in notice
             ):
                 if "Must obtain written permission from Editor" in notice["journalSherpaPreRest_s"]:
                     notice["preprint_embargo"] = "perm_from_editor"
@@ -418,7 +331,8 @@ def should_be_open(notice):
 
     return notice["postprint_embargo"], notice["preprint_embargo"]
 
-def creeFichesExpertise(idx, idHal, aureHal, lstDom) :
+
+def creeFichesExpertise(idx, idHal, aureHal, lstDom):
     # AUCUN des deux scope ne remontent toutes les fiches domaine chercheur : elles n'existent pas. GRRRR
     query = esActions.scope_all()
     count = es.count(index="domaine_hal_referentiel", query=query)["count"]
@@ -446,7 +360,7 @@ def creeFichesExpertise(idx, idHal, aureHal, lstDom) :
         # on créé les entrées ici et on marque le problème du référentiel dans le champ refOk
         domaine = dict()
         domaine["id"] = new
-        domaine ["label_fr"] = new
+        domaine["label_fr"] = new
         domaine["label_en"] = new
         newFiche = utils.creeFiche(domaine)
         newFiche['idhal'] = idHal
@@ -455,20 +369,20 @@ def creeFichesExpertise(idx, idHal, aureHal, lstDom) :
         elastic_id = f"{idHal}.{newFiche['chemin']}"
         newFiche['sovisu_id'] = elastic_id
         newFiche['refOk'] = False
-        newFiche['level'] = newFiche['chemin'].count('.')   # champ pour affichage... pas trouvé mieux
+        newFiche['level'] = newFiche['chemin'].count(
+            '.')  # champ pour affichage... pas trouvé mieux
         newFiche['origin'] = "datagouv"
-        #print("Nouveau dans le dico ??? çà sort d'où ?", new)
+        # print("Nouveau dans le dico ??? çà sort d'où ?", new)
         es.index(index=idx, id=elastic_id, document=json.dumps(newFiche), refresh="wait_for", )
 
     # Ci dessous peut être grandement simplifié
     # il suffit de trouver la requête sur es qui donne un match exact (sur le champ chemin) pour tous les Vu
     for dom in Vu:
-        lstReq = []
-        lstReq.append({
+        lstReq = [{
             "match": {
                 "chemin": dom
             }
-        })
+        }]
 
         req = {
             "bool": {
@@ -484,20 +398,22 @@ def creeFichesExpertise(idx, idHal, aureHal, lstDom) :
 
         resDomainesRef = es.search(index="domaine_hal_referentiel", query=req)
         for fiche in resDomainesRef['hits']['hits']:
-            if fiche['_source']['chemin'] == dom: # si la req pouvait donner un match exact, on se passerait de çà
+            if fiche['_source'][
+                'chemin'] == dom:  # si la req pouvait donner un match exact, on se passerait de çà
                 newFiche = fiche['_source']
                 newFiche['validated'] = False  # domaines pas validés par défaut
                 # Id proposition : valider les domaines par défaut et laisser la possibilité d'en valider d'autres par explorateur d'arbre ?
                 newFiche['idhal'] = idHal  # taggage, l'idhal sert de clé
-                newFiche['aurehal'] = aureHal # un domaine est attaché à l'aurehalId
+                newFiche['aurehal'] = aureHal  # un domaine est attaché à l'aurehalId
                 # Et rajouts besoins spécifiques (genre précisions / notes...)
                 elastic_id = f"{idHal}.{newFiche['chemin']}"
                 newFiche['sovisu_id'] = elastic_id
                 newFiche['level'] = dom.count('.')  # champ pour affichage... pas trouvé mieux
-                newFiche['refOk'] = True    # champ pour désigner les pb du référentiel
+                newFiche['refOk'] = True  # champ pour désigner les pb du référentiel
                 # Puis on indexe la fiche
                 newFiche['origin'] = "datagouv"
-                es.index(index=idx, id=elastic_id, document=json.dumps(newFiche), refresh="wait_for",)
+                es.index(index=idx, id=elastic_id, document=json.dumps(newFiche),
+                         refresh="wait_for", )
 
 
 # TODO: Refactor that function
@@ -518,30 +434,29 @@ def create_searcher_concept_notices(idhal, aurehal):
                                     size=expertises_count)
     searcher_expertises = searcher_expertises["hits"]["hits"]
 
-
     if len(chercheur_concept) > 0:
 
-        idDomainChecheur =[]
+        idDomainChecheur = []
         for concept1 in chercheur_concept['children']:
-            idDomainChecheur .append(concept1['id'])
+            idDomainChecheur.append(concept1['id'])
             if 'children' in concept1.keys():
                 for concept2 in concept1['children']:
-                    idDomainChecheur. append(concept2['id'])
-                    if 'children' in concept2.keys(): # pas sûr du besoin de ce niveau là
+                    idDomainChecheur.append(concept2['id'])
+                    if 'children' in concept2.keys():  # pas sûr du besoin de ce niveau là
                         for concept3 in concept2['children']:
                             idDomainChecheur.append(concept3['id'])
-        #idDomainChecheur = [exp['id'] for exp in chercheur_concept['children']]
+        # idDomainChecheur = [exp['id'] for exp in chercheur_concept['children']]
 
         # Gestion des existants
         dejaLa = [fiche for fiche in searcher_expertises if
                   fiche["_source"]['chemin'].replace("domAurehal.", "") in idDomainChecheur]
 
         for fiche in dejaLa:
-            if fiche["_source"]['origin']!= "datagouv":
+            if fiche["_source"]['origin'] != "datagouv":
                 fiche["_source"]['origin'] = "datagouv"
-                #fiche["_source"]['validated'] = True
+                # fiche["_source"]['validated'] = True
                 es.update(index=SV_INDEX, id=fiche["_id"], body=fiche["_source"])
-            idDomainChecheur .remove(fiche["_source"]['chemin'].replace("domAurehal.", ""))
+            idDomainChecheur.remove(fiche["_source"]['chemin'].replace("domAurehal.", ""))
         # les autres on les créé
         creeFichesExpertise(idx=SV_INDEX, idHal=idhal, aureHal=aurehal, lstDom=idDomainChecheur)
 
