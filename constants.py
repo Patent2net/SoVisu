590b4b51370ee6c5af16240a1c9bbe44886000c6
E-Bara <62282795+E-Bara@users.noreply.github.com>
2023-08-24 11:40:25 +0200

    Update environment configurations in constants.py
    Replaced hardcoded strings with environment variable calls in 'constants.py'. The changes allow for external setting of KIBANA_URL and both index constants, without modifying the source code. Default values are still included in the case that these environment variables aren't declared. This enhances flexibility and supports different configurations for local development and production environment.

--- constants.py
+++ constants.py
@@ -1,13 +1,17 @@
+import os
+
 from dateutil import tz
 
-KIBANA_URL = "/kibana"
+# Define Kibana Parameter
+KIBANA_URL = os.getenv('KIBANA_URL', "/kibana")  # default value is "/kibana"
 
-SV_INDEX = "sovisu_index"
+# Define Index Constants
+SV_INDEX = os.getenv('SV_INDEX', "sovisu_index")  # default value is "sovisu_index"
+SV_LAB_INDEX = os.getenv('SV_LAB_INDEX', "sovisu_index")  # default value is "sovisu_index"
 
-SV_LAB_INDEX = "sovisu_index"
-
+# Define References Constants
 SV_HAL_REFERENCES = "domaine_hal_referentiel"
-
 SV_STRUCTURES_REFERENCES = "structures_directory"
 
+# Timezone Configuration
 TIMEZONE = tz.gettz('Europe/Paris')
